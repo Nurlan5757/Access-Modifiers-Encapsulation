db64ccce20a098d9d8c69410d2782f7d62cb0929
unknown <tu4pmkvys@code.edu.az>
2024-04-03 03:23:22 +0400

    tapsiriq

---
+++ Access Modifiers, Encapsulation/Models/Weapon.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers__Encapsulation.Models
+{
+    internal class Weapon
+    {
+        private int _bulletCapacity;
+        private int _bulletCount;
+        private double _combDischargeSecond;
+        private bool _shootingMode=true;
+
+
+        public int BulletCapacity
+        {
+            get 
+            { 
+                return _bulletCapacity; 
+            }
+            set
+            {
+                if(value > 0)
+                { 
+                    _bulletCapacity = value;
+                   
+                }
+                else
+                {
+                    Console.WriteLine("yanlisdir");
+                   
+                }
+            }
+        }
+
+        public int BulletCount
+        {
+            get
+            {
+                return _bulletCount;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _bulletCount = value;  
+                    
+                }
+               /* else
+                {
+                    Console.WriteLine("gulle bitdi");
+                }*/
+               
+            }
+        }
+
+        public double CombDischargeSecond
+        {
+            get
+            {
+                return _combDischargeSecond;
+            }
+            set
+            {
+                if (value > 0) 
+                { 
+                    _combDischargeSecond = value;
+                  
+                }
+                
+            }
+        }
+
+        public bool ShootingMode
+        {
+            get
+            {
+                return _shootingMode;
+            }
+            set
+            {
+               
+                
+            }
+
+        }
+
+
+
+        public Weapon(int bulletCapacity, int bulletCount, double combDischargeSecond, bool shootingMode)
+        {
+            BulletCapacity = bulletCapacity;
+            BulletCount = bulletCount;
+            CombDischargeSecond = combDischargeSecond;
+            ShootingMode = shootingMode;
+        }
+
+
+        public void Shoot()
+        {
+            
+                if (BulletCount >0)
+                {
+                    BulletCount--;
+                
+                    
+                }
+            Console.WriteLine("piyu piyu");
+
+        }
+
+        public void Fire()
+        { 
+           
+                while (BulletCount !=0 )
+                {
+                    BulletCount--;
+                    CombDischargeSecond++;
+                break;
+                      
+                }
+            Console.WriteLine(CombDischargeSecond++);
+
+
+        }
+
+        public void GetRemainBulletCount()
+        {
+            
+                while (BulletCapacity != BulletCount)
+                {
+                    BulletCount++;
+                   break;
+                }
+            Console.WriteLine(BulletCount);
+
+
+        }
+
+        public void Reload()
+        {
+
+        }
+
+        public void ChangeFireMode()
+        {
+
+        }
+
+        
+    }
+}
---
+++ Access Modifiers, Encapsulation/Program.cs
@@ -0,0 +1,65 @@
+﻿using Access_Modifiers__Encapsulation.Models;
+using System.Reflection.Metadata;
+
+namespace Access_Modifiers__Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weapon a = new Weapon(30,10,0);
+            a.Shoot();
+
+            a.Fire();
+
+            a.GetRemainBulletCount();
+
+
+
+            do
+            {
+                Console.WriteLine(@"0. İnformasiya almaq üçün
+1. Shoot metodu üçün
+2. Fire metodu üçün
+3. GetRemainBulletCount metodu üçün
+4. Reload metodu üçün
+5. ChangeFireMode metodu üçün
+6. Proqramdan dayandırmaq üçün
+qısayoldur.
+7. Redaktə et");
+
+
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 0:
+                        İnformasiya();
+                        break;
+                    case 1:
+                        Shoot();
+                        break;
+                    case 2:
+                        Fire();
+                        break;
+                    case 3:
+                        GetRemainBulletCount();
+                        break;
+                    case 4:
+                        Reload();
+                        break;
+                    case 5:
+                        ChangeFireMode();
+                        break;
+                    case 6:
+                        Proqramdan dayandırmaq();
+                        break;
+                    case 7:
+                }
+
+            } while (isContinue);
+            Console.Clear();
+            Console.WriteLine();
+        }
+    }
+}
